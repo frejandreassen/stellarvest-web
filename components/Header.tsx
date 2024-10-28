70e1a4523bf792674698f0e9e4d97f763ad74278
Luis Dixon <luis.dixon@gmail.com>
2024-10-28 16:31:38 +0000

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header(){
                 <span className="sr-only">StellarVest Asset Management</span>
                 <img
                   className="h-12 w-auto"
-                  src="/logo-w2.png"
+                  src="/icon.png"
                   alt="StellarVest logo"
                 />
               </a>
@@ -55,7 +55,7 @@ export default function Header(){
                   <span className="sr-only">StellarVest Asset Management</span>
                   <img
                     className="h-12 w-auto"
-                    src="/logo-w2.png"
+                    src="/icon.png"
                     alt="StellarVest Logo
                     "
                   />
