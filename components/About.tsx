c697e1c238160031fbb42cfa947af223e2699cbf
Luis Dixon <luis.dixon@gmail.com>
2024-10-30 14:18:13 +0000

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -59,8 +59,8 @@ export default function About() {
                       />
               </div>
              </div>
-          </div>
-            {/*<div className="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8"> 
+          {/*</div>
+            <div className="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8"> 
             <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
                    <div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto"> 
                       <img
