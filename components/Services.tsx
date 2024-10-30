372b3727e5eb783d49bdf7c8f5c735370197b312
Luis Dixon <luis.dixon@gmail.com>
2024-10-30 18:03:58 +0000

    Update Services.tsx

--- components/Services.tsx
+++ components/Services.tsx
@@ -19,7 +19,7 @@ const services = [
     ],
     href: 'https://realestate.stellarvest.pt/',
     icon: BuildingOffice2Icon,
-    label:'Para aceder aos imóveis que temos para comercialização, clique no '
+    label:'Para aceder aos imóveis que temos para comercialização, clique no ',
     Link: 'Link'
   },
   {
