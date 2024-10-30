c77920018c4d676dac5e982fd9f1c1327ed2cdc5
Luis Dixon <luis.dixon@gmail.com>
2024-10-30 11:55:30 +0000

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -54,7 +54,7 @@ export default function About() {
           </div>
           <img
                 alt=""
-                src="\business-people-analyzes-graphs-and-charts-to-exa-2023-11-27-05-32-43-utc"
+                src="\business-people-analyzes-graphs-and-charts-to-exa-2023-11-27-05-32-43-utc.png"
                 className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
           />
         </div>
