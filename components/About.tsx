aee3b7ae9f28968636a5ff73b20209dde781b3aa
Luis Dixon <luis.dixon@gmail.com>
2024-10-30 12:54:38 +0000

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -54,13 +54,13 @@ export default function About() {
             </div>
             {/*<div className="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8"> */}
             <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
-                  <div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto">
+                {/*   <div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto"> */}
                     <img
                       alt=""
                       src="\business-people-analyzes-graphs-and-charts-to-exa-2023-11-27-05-32-43-utc.png"
                       className="aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
                     />
-              </div>
+             {/* </div>*/}
         </div>
 
         {/* Quem Somos section with images */}
