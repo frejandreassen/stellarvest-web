5824997b77caa6977a9ff4d5af645b04a308d690
frejandreassen <frej.andreassen@gmail.com>
2025-04-07 11:46:27 +0200

    Change links in hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -55,12 +55,12 @@ export default function Hero() {
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
-                  href="#about"
+                  href="/about"
                   className="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-foreground focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   {t('learnMore')}
                 </a>
-                <a href="#contact" className="text-sm font-semibold leading-6 text-white">
+                <a href="/contact" className="text-sm font-semibold leading-6 text-white">
                   {t('contactUs')} <span aria-hidden="true">→</span>
                 </a>
               </div>
